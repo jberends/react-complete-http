3a34dc1cad95efbe55337897df93a17e0849d4a3
Jochem Berends <jochem.berends@ke-works.com>
2018-09-07 16:46:46 +0200

    sec9 les152 only limit to 4 posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,7 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts : []
+        posts: []
     }
 
     componentDidMount() {
@@ -16,13 +16,23 @@ class Blog extends Component {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then((response) => {
                 // console.log(response)
-                this.setState({posts: response.data})
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Jochem'
+                    }
+                })
+                this.setState({ posts: updatedPosts })
             })
     }
 
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author} />
         });
 
         return (
