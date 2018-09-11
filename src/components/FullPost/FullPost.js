0761b0d9ac61b358e2218492cc5c1f4961374ca2
Jochem Berends <jochem.berends@ke-works.com>
2018-09-11 17:17:38 +0200

    sec9 les154 upgrades

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+    componentDidUpdate() {
+        if (this.props.id) {
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {console.log(response)})
+        }
+    }
+
+    render() {
+        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         post = (
             <div className="FullPost">
                 <h1>Title</h1>
