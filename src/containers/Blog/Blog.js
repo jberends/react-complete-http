60f3c7ca9af4eb933b5a52856d7e7c89dcf0c927
Jochem Berends <jochem.berends@ke-works.com>
2018-09-19 17:43:05 +0200

    sec9 les158 catching errors to log or change state

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -26,20 +26,27 @@ class Blog extends Component {
                 })
                 this.setState({ posts: updatedPosts })
             })
+            .catch(error => {
+                console.log(error)
+                this.setState({ error: true })
+            })
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
+        this.setState({ selectedPostId: id })
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{ textAlign: 'center', color: 'red' }}>Something went wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
 
         return (
             <div>
