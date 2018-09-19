521090c9495f1dfbe664dd9a9415b1e7c098d2ac
Jochem Berends <jochem.berends@ke-works.com>
2018-09-19 17:50:27 +0200

    sec9 les 159 axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log('[REQ]',request);
+    // possibly add auth headers here.
+    return request;
+}, error => {
+    console.log('[ERROR]',error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log('[RES]',response);
+    return response;
+}, error => {
+    console.log('[ERROR]',error);
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
